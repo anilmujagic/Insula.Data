f858de960ddc52cc86f4d3212b24688c5a31db2c
Anil Mujagic <anil.mujagic@gmail.com>
2013-04-30 00:06:23 +0200

    Create Insula.DataAnnotations project

---
+++ Source/Insula.DataAnnotations/Schema/IdentityAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Insula.DataAnnotations.Schema
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class IdentityAttribute : Attribute
+    {
+    }
+}
