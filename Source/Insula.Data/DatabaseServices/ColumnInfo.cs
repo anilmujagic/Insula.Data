baa4e66b8162e9dc9b36382158f00a7a4b0c9a4e
Anil Mujagic <anil.mujagic@gmail.com>
2013-04-30 00:31:20 +0200

    Add DB schema extraction classes

---
+++ Source/Insula.Data/DatabaseServices/ColumnInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insula.Data.DatabaseServices
+{
+    public class ColumnInfo
+    {
+        internal ColumnInfo()
+        {
+        }
+
+        public string Name { get; internal set; }
+        public string DataType { get; internal set; }
+        public int? MaxLength { get; internal set; }
+        public int? DecimalPlaces { get; internal set; }
+        public bool IsNullable { get; internal set; }
+        public bool IsPrimaryKey { get; internal set; }
+        public bool IsIdentity { get; internal set; }
+        public bool IsComputed { get; internal set; }
+        public int OrdinalPosition { get; internal set; }
+    }
+}
---
+++ Source/Insula.Data/DatabaseServices/TableInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insula.Data.DatabaseServices
+{
+    public class TableInfo
+    {
+        internal TableInfo()
+        {
+            _columns = new List<ColumnInfo>();
+        }
+
+        public string Name { get; internal set; }
+        public string Schema { get; internal set; }
+        public string Database { get; internal set; }
+
+        private List<ColumnInfo> _columns;
+        public IEnumerable<ColumnInfo> Columns
+        {
+            get
+            {
+                return _columns.AsReadOnly();
+            }
+        }
+
+        internal void AddColumn(ColumnInfo columnInfo)
+        {
+            _columns.Add(columnInfo);
+        }    
+    }
+}
