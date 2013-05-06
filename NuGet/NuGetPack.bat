a07de2f35ae29327b10f9a4c41b5ce4a7e7323d5
Anil Mujagic <anil.mujagic@gmail.com>
2013-05-07 00:37:14 +0200

    Make NuGet package for Insula.DataAnnotations

--- NuGet/NuGetPack.bat
+++ NuGet/NuGetPack.bat
@@ -1,1 +1,2 @@
 nuget pack ..\Source\Insula.Data\Insula.Data.csproj -Symbols -Prop Configuration=Release
+nuget pack ..\Source\Insula.DataAnnotations\Insula.DataAnnotations.csproj -Symbols -Prop Configuration=Release
